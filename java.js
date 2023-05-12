d87f7b87432b703ba6b0615b3f678c68cf49e51d
Ameh Sunday <Ameh.S@bullseye.local>
2023-05-12 15:07:03 +0100

    second commit

---
+++ java.js
@@ -0,0 +1,36 @@
+//  get a reference to the home link
+// const homeLink = document.getElementById("home-link")
+
+
+
+
+  
+  
+  
+  
+  const links = document.querySelectorAll('nav ul li a');
+  
+  links.forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent default link behavior
+      const targetId = link.getAttribute('href');
+      const target = document.querySelector(targetId);
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  });
+  
+  const form = document.getElementById("myForm");
+  from.addEventListener("submit", function(event) {
+    event.preventDefault();
+  
+    const name = form.elements["name"].value;
+    const email = form.elements["email"].value;
+    const message = form.elements["message"].value;
+  
+    if (name === "" || email === "" || message === "") {
+      alert("Please fill out all fields")
+      return;
+    }
+  
+    form.submit();
+  });
